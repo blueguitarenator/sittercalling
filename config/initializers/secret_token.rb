ab7367b9f8e8e7c2223c5733751fa9ef7d3a0fcb
Rich Johnson <blueguitarenator@gmail.com>
2011-07-02 09:05:58 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sittercalling::Application.config.secret_token = 'abfb758d3cbc55aaac773fbbcfbf4e029c77ec0ddbf670f7ad280e202b3b9f4141c42051da52e71d9c95b7ea08bf8ae01f40923b8c669e80af5edd77df67c906'
