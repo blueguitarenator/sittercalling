b8bdc2f4b85cb471f642fd6daa722d6167b802fc
Rich Johnson <blueguitarenator@gmail.com>
2011-07-03 09:17:28 -0500

    fix deploy file?

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,8 +8,8 @@ set :user, 'mybabysi'
 set :use_sudo, false
 set :branch, "master"
 set :deploy_to, "/home/mybabysi/#{application}"
-set :deploy_via, :checkout
-default_run_options[:pty] = true
+set :deploy_via, :remote_cache
+
 
 role :web, "jason.asmallorange.com"                          # Your HTTP server, Apache/etc
 role :app, "jason.asmallorange.com"                          # This may be the same as your `Web` server
@@ -20,8 +20,13 @@ role :db,  "jason.asmallorange.com", :primary => true # This is where Rails migr
 
 # If you are using Passenger mod_rails uncomment this:
 
+after "deploy", "deploy:bundle_gems"
+after "deploy:bundle_gems", "deploy:restart"
 
 namespace :deploy do
+  task :bundle_gems do
+    run "cd ${deploy_to}/current && bundle install vendor/gems"
+  end
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
