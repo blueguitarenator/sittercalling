4e73f637f02a141fc1807dad21dea269d1a138a9
Rich Johnson <blueguitarenator@gmail.com>
2011-08-23 07:00:49 -0500

    fix routing for home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,7 +56,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         flash[:notice] = "User #{@user.last_name} was registered successfully."
-        format.html { redirect_to(root_url) }
+        format.html { redirect_to(@user) }
         format.xml  { render :xml => @user, :status => :created, :location => @user }
       else
         format.html { render :action => "new" }
