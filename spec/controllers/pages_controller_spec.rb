22b8669158805c77b0954c54725ce3c9ba061e7c
Rich Johnson <blueguitarenator@gmail.com>
2011-08-23 18:26:55 -0500

    fix signup routes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -12,24 +12,12 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
-    
-    it "should have the right title" do
-      get 'home'
-      # response.should have_selector('title', 
-                                    # :content => @base_title + " | Home")
-    end
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
-    end
-    
-    it "should have the right title" do
-      get 'contact'
-      # response.should have_selector("title", 
-                                    # :content => @base_title + " | Contact")
     end
   end
   
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -4,7 +4,25 @@ describe "LayoutLinks" do
   it "should have a Home page at '/'" do
     get '/'
     # response.should have_selector('head title', :content => "Home") 
-    page.should have_selector("#home #container")
+    response.should be_success
+  end
+  
+  it "should have a Contact page at '/contact'" do
+    get '/contact'
+    # response.should have_selector('head title', :content => "Home") 
+    response.should be_success
+  end
+ 
+  it "should have a Contact page at '/about'" do
+    get '/about'
+    # response.should have_selector('head title', :content => "Home") 
+    response.should be_success
+  end
+ 
+  it "should have a Contact page at '/signup'" do
+    get '/signup'
+    # response.should have_selector('head title', :content => "Home") 
+    response.should be_success
   end
 
 end
