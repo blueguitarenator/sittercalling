5813ec54f234b0eef7071c408164e63e32521073
Rich Johnson <blueguitarenator@gmail.com>
2011-09-13 19:04:42 -0500

    fix reset password and fix some cukes

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,8 +27,6 @@ class User < ActiveRecord::Base
   
   has_many :friendships
   has_many :friends, :through => :friendships
-#  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
-#  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
   
   has_many :events, :dependent => :destroy
   has_many :replies, :dependent => :destroy
